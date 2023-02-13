77e433ea2abf3de03349b74537487a02a72f7310
www.rubiluqman7@gmail.com <www.rubiluqman7@gmail.com>
2023-02-14 01:35:33 +0300

    completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,28 @@
-require 'pry'
 
 class String
 
   def sentence?
-    
+
+    self.end_with?(".") ? true : false
   end
 
   def question?
 
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
 
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
 
+    word = self
+    word.split(/\?|\.|!/).reject(&:empty?).length
   end
 end
+end
+
+puts "The weather outside is frightful.".sentence?
+puts "Did you eat today?".sentence?
